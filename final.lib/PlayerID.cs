e75e7abdb31202607d5101d2b06a2231f18b170e
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-04-11 20:56:13 -0600

    More things fixed and more things brocken

---
+++ final.lib/PlayerID.cs
@@ -0,0 +1,19 @@
+namespace final.lib;
+
+public abstract class PlayerID
+{
+    public PlayerID(string name)
+    {
+        name = name;
+    }
+
+    public string Name {get;}
+    public decimal Points {get; private set;}
+
+    public static List<PlayerID> Load(string path)
+    {
+        var players = new List<PlayerID>();
+        var PlayerName = "";
+        var points = 0;
+    }
+}
